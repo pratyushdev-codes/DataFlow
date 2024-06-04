9d0491a19156f6cad34020d152bb87d13f4439ae
Pratyush Birole <birole.pratyush@gmail.com>
2024-06-05 01:25:33 +0530

    Add files via upload

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Navbar from "@/components/global/Navbar";
 import Image from "next/image";
+import { LampComponent } from "@/components/global/lamp";
 import { clients, products } from "@/lib/constants";
 import { ContainerScroll } from "@/components/global/container-scroll-animation";
 import { Button } from "@/components/ui/button";
 import { InfiniteMovingCards } from "@/components/global/infinte-moving-cards";
 import { HeroParallax } from "@/components/global/connect-parallax";
+import { Section } from "lucide-react";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
@@ -40,6 +42,9 @@ export default function Home() {
       <section>
          <HeroParallax products={products}></HeroParallax>
       </section>
+      <Section>
+        <LampComponent/>
+      </Section>
     </main>
   );
 }
